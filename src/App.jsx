7f9d2995fd13b81f9e8d434e643c920b00cbb350
Eduardo Henrique <eduarohenrique8148@example.com>
2023-06-05 01:08:19 -0300

    criei a tela de historia

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import Pagina_Personagem from './components/Pagina_Personagem'
+import Pagina_Historia from './components/Pagina_Historia'
+import CommentBar from './components/CommentBar'
 import './App.css'
 
 function App() {
- 
+  const [comments, setComments] = useState([]);
+
+  const addComment = (newComment) => {
+    setComments([...comments, newComment]);
+  };
+
   return (
     <>
-     <Pagina_Personagem/>
+      <Pagina_Historia/>
+      <div>
+        <CommentBar addComment={addComment} />
+        <div>
+          {comments.map((comment, index) => (
+            <div key={index}>{comment}</div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
